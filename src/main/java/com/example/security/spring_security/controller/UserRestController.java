3cd56a9a43cdb06888264d42e2474cc628adce19
rgobov <rgobov@gmail.com>
2025-03-16 12:58:28 +0200

    Поправил мелочи

--- src/main/java/com/example/security/spring_security/controller/UserRestController.java
+++ src/main/java/com/example/security/spring_security/controller/UserRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 public class UserRestController {
 
     private final UserService userService;
@@ -24,45 +24,38 @@ public class UserRestController {
     }
 
     @GetMapping("api/users")
-    @ResponseBody
     public List<User> getAllUsers() {
         return userService.findAll();
     }
 
     @GetMapping("api/users/{id}")
-    @ResponseBody
     public User getUserById(@PathVariable Long id) {
         return userService.findById(id);
     }
 
     @PostMapping("api/users")
-    @ResponseBody
     public User createUser(@RequestBody User user) {
         Set<Long> roleIds = user.getRoles().stream().map(Role::getId).collect(Collectors.toSet());
         return userService.createUser(user, roleIds);
     }
 
     @PutMapping("api/users/{id}")
-    @ResponseBody
     public User updateUser(@PathVariable Long id, @RequestBody User user) {
         Set<Long> roleIds = user.getRoles().stream().map(Role::getId).collect(Collectors.toSet());
         return userService.updateUser(id, user.getFirstName(), user.getLastName(), user.getAge(), user.getEmail(), user.getPassword(), roleIds);
     }
 
     @DeleteMapping("api/users/{id}")
-    @ResponseBody
     public void deleteUser(@PathVariable Long id) {
         userService.delete(id);
     }
 
     @GetMapping("api/users/current")
-    @ResponseBody
     public User getCurrentUser() {
         return userService.getCurrentUser();
     }
 
     @GetMapping("api/roles")
-    @ResponseBody
     public List<Role> getAllRoles() {
         return userService.findAll().stream()
                 .flatMap(u -> u.getRoles().stream())
