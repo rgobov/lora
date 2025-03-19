aa55cc1735e63d5d7eb28bd714d3707689dcd693
rgobov <rgobov@gmail.com>
2025-03-19 14:48:57 +0200

    Поправил секьюрити конфиг

--- src/main/java/com/example/security/spring_security/configs/WebSecurityConfig.java
+++ src/main/java/com/example/security/spring_security/configs/WebSecurityConfig.java
@@ -1,30 +1,34 @@
 package com.example.security.spring_security.configs;
 
 import com.example.security.spring_security.seurity.AuthProviderImpl;
-import com.example.security.spring_security.seurity.CustomsDetailsServise;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.security.spring_security.seurity.CustomDetailsServise;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    @Autowired
-    LoginSuccessHandler loginSuccessHandler;
+
+    private final LoginSuccessHandler loginSuccessHandler;
+
+    public WebSecurityConfig(LoginSuccessHandler loginSuccessHandler) {
+        this.loginSuccessHandler = loginSuccessHandler;
+    }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) throws Exception {
         http
+                .authenticationProvider(authenticationProvider)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/error","/api/users/**","/admin/**","roles").permitAll()
-                        .requestMatchers("/user","/mainPage").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers("/admin/**").hasAnyRole("ADMIN","USER")
+                        .requestMatchers("/login", "/error", "/api/users/**", "/admin/**", "roles").permitAll()
+                        .requestMatchers("/user", "/mainPage").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/admin/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
@@ -46,19 +50,17 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider() {
-        return new AuthProviderImpl(customsDetailsServise(), passwordEncoder());
+    public AuthenticationProvider authenticationProvider(CustomDetailsServise customDetailsServise, BCryptPasswordEncoder passwordEncoder) {
+        return new AuthProviderImpl(customDetailsServise, passwordEncoder);
     }
 
     @Bean
-    public CustomsDetailsServise customsDetailsServise() {
-        return new CustomsDetailsServise();
+    public CustomDetailsServise customsDetailsServise() {
+        return new CustomDetailsServise();
     }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-
 }
