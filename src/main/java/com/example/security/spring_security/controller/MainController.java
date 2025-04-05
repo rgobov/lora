edd29cb6485755bdbdff773e9f8469deff99a850
rgobov <rgobov@gmail.com>
2025-04-05 14:29:04 +0200

    Добавил страницы мои работы и обо мне

--- src/main/java/com/example/security/spring_security/controller/MainController.java
+++ src/main/java/com/example/security/spring_security/controller/MainController.java
@@ -2,6 +2,7 @@ package com.example.security.spring_security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class MainController {
 
@@ -12,17 +13,26 @@ public class MainController {
 
     @GetMapping("/rf-microneedling")
     public String microneedling() {
-        return "lora/rf-microneedling"; // Возвращает имя HTML-файла (smas-lifting.html)
+        return "lora/rf-microneedling"; // Возвращает имя HTML-файла (rf-microneedling.html)
     }
 
     @GetMapping("/mediderma-skincare")
     public String mediderma() {
-        return "lora/mediderma-skincare"; // Возвращает имя HTML-файла (smas-lifting.html)
+        return "lora/mediderma-skincare"; // Возвращает имя HTML-файла (mediderma-skincare.html)
     }
 
     @GetMapping("/chiromassage")
     public String chiromassage() {
         return "lora/chiromassage"; // Имя шаблона без расширения .html
     }
-}
 
+    @GetMapping("/about")
+    public String about() {
+        return "lora/about"; // Возвращает имя HTML-файла (about.html)
+    }
+
+    @GetMapping("/works")
+    public String works() {
+        return "lora/works"; // Возвращает имя HTML-файла (works.html)
+    }
+}
